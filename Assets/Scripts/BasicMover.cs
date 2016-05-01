b2cb9fb292db802dac01b1358784a44006373f14
Chris Sun <chris438@gmail.com>
2016-05-01 10:29:28 -0700

    Initial commit for box shooter

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMover : MonoBehaviour {
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+	public bool doMotion = false;
+	public bool doSpin = true;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotate 
+		if(doSpin) {
+			gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+		}
+		//move up and down
+		if(doMotion) {
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAgain : MonoBehaviour {
+	public string levelToPlayAgain;
+	// respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the RestartGame function in the game manager
+			GameManager.gm.RestartGame(levelToPlayAgain);
+		}
+	}
+}
